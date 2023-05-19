50516ef19f7fb75a2e7b911e448333a143ea38a4
Cheslyn Herman <cheslynherman1169@gmail.com>
2023-05-19 13:18:36 +0200

    calculator push

---
+++ assets/index.js
@@ -0,0 +1,21 @@
+const calculatorScreen= document.getElementById(calc-screen);
+
+function calcNum() {
+    display.value
+}
+
+function calcOp() {
+
+}
+
+function calcClear(){
+
+}
+
+function calcDel(){
+
+}
+
+function calcEval() {
+    
+}
